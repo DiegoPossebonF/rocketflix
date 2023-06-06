e89e0099f267a80cb59ab775000a9c602af81c50
dpfdev <diegopossebonf@gmail.com>
2023-06-06 08:55:28 -0300

    Add axios getMovie

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
+import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js"
 
-const btChangeMovie = document.querySelector("#change-movie");
-btChangeMovie.addEventListener("click", getMovie);
+const btFind = document.querySelector("#bt-find")
+btFind.addEventListener("click", getMovie)
 
 function getMovie() {
-
-  const id_movie = 888;
-  const url = `${BASE_URL}${id_movie}?${API_KEY}&${language}`;
+  const id_movie = Math.floor(Math.random() * 1000 + 1)
+  const url = `${BASE_URL}${id_movie}?${API_KEY}&${language}`
 
   axios
     .get(url)
     .then((response) => {
-      const div_movie = document.querySelector("#movie");
-      div_movie.innerHTML = `<img src="${IMG_URL}${response.data.poster_path}" />`
+      const poster = document.querySelector(".poster")
+      const title = document.querySelector(".title")
+      const description = document.querySelector(".description")
+      poster.src = `${IMG_URL + response.data.poster_path}`
+      title.innerHTML = response.data.title
+      description.innerHTML = response.data.overview
+      console.log(response.data)
     })
     .catch((error) => {
       console.log(error)
