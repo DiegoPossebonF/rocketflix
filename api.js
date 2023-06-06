b8b37165aee1d6ac5dd2b8b85426b64eb8b62278
dpfdev <diegopossebonf@gmail.com>
2023-06-05 23:55:21 -0300

    testing api with axios

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 // Substitua o CHAVE_DA_API com a chave gerada no site.
 
-export const API_KEY = 'api_key=CHAVE_DA_API';
+export const API_KEY = 'api_key=038278e497275716240f0e6063b8e8b1';
 export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 export const language = 'language=pt-BR';
--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-import {
-  API_KEY, BASE_URL,
-  IMG_URL,
-  language,
-} from './api.js'
+import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
+
+const btChangeMovie = document.querySelector("#change-movie");
+btChangeMovie.addEventListener("click", getMovie);
+
+function getMovie() {
+
+  const id_movie = 888;
+  const url = `${BASE_URL}${id_movie}?${API_KEY}&${language}`;
+
+  axios
+    .get(url)
+    .then((response) => {
+      const div_movie = document.querySelector("#movie");
+      div_movie.innerHTML = `<img src="${IMG_URL}${response.data.poster_path}" />`
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
