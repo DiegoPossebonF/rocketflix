b8345d9b587a5664f6065fe5186a35f3852cafce
dpfdev <diegopossebonf@gmail.com>
2023-06-09 17:23:22 -0300

    add error not finding movie

--- main.js
+++ main.js
@@ -6,19 +6,22 @@ btFind.addEventListener("click", getMovie)
 function getMovie() {
   const id_movie = Math.floor(Math.random() * 1000 + 1)
   const url = `${BASE_URL}${id_movie}?${API_KEY}&${language}`
-
+  const poster = document.querySelector(".poster")
+  const title = document.querySelector(".title")
+  const description = document.querySelector(".description")
   axios
     .get(url)
     .then((response) => {
-      const poster = document.querySelector(".poster")
-      const title = document.querySelector(".title")
-      const description = document.querySelector(".description")
       poster.src = `${IMG_URL + response.data.poster_path}`
       title.innerHTML = response.data.title
       description.innerHTML = response.data.overview
       console.log(response.data)
     })
     .catch((error) => {
+      console.log("ERROR!!")
       console.log(error)
+      poster.src = "assets/poster-error.jpg"
+      title.innerHTML = "Ops, hoje não é dia de assistir filme. Bora codar! 🚀"
+      description.innerHTML = ""
     })
 }
